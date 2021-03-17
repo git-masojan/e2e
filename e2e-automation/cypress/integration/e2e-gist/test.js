f60788428ca8514224961159b89d32bbedc1acb1
git-masojan <janoko.dewanto@gmail.com>
2021-03-18 04:00:58 +0700

    e2e-gist test

--- e2e-automation/cypress/integration/e2e-gist/test.js
+++ e2e-automation/cypress/integration/e2e-gist/test.js
@@ -1,33 +1,31 @@
 /// <reference types="cypress" />
-const username = process.env.username
-const password = process.env.password
 //email dan password diisi dengan credential usernya
 
-context('Actions', () => {
+context('Gits Scenario', () => {
     beforeEach(() => {
-      cy.visit('https://github.com/login')
+      cy.visit(Cypress.env('base_url')+'/login')
       cy.get('input[id="login_field"]')
-        .type(username).should('have.value', username)
+        .type(Cypress.env('username')).should('have.value', Cypress.env('username'))
       cy.get('input[id="password"]')
-        .type(password).should('have.value', password)
+        .type(Cypress.env('password')).should('have.value', Cypress.env('password'))
       cy.get('input[name="commit"]').click()
     })
   
     // https://on.cypress.io/interacting-with-elements
   
-    it('.type() - type into a DOM element', () => {
-      // https://on.cypress.io/type
-      cy.visit('https://gist.github.com/')
+    it('Create, Edit, Delete, See list', () => {
+      cy.visit(Cypress.env('gits_url'))
       //create gist
-      cy.get('input[name="gist[description]"]').type('Create Gist')
-      cy.get('input[name="gist[contents][][name]"]').type('Creating Gist')
-      cy.get('pre[class=" CodeMirror-line "]').type('Creating Gist')
-      cy.get('button[class="btn js-gist-create "]').click()
+      cy.get('input[name="gist[description]"]').type('Gist description…')
+      cy.get('input[name="gist[contents][][name]"]').type('Filename including extension….md')
+      cy.get('pre[class=" CodeMirror-line "]').type('Filename including extension….md')
+      cy.get('button[class="btn btn-primary BtnGroup-item hx_create-pr-button js-sync-select-menu-button"]').click()
       //edit gist
       cy.get('a[aria-label="Edit this Gist"]').click()
-      cy.get('input[name="gist[description]"]').type('This is deescription for creating gist')
+      cy.get('input[name="gist[description]"]').type('Gist description… Edit')
       cy.get('button[class="btn btn-primary"]').click()
       //delete gist
       cy.get('button[class="btn btn-sm btn-danger"]').click()
+      cy.visit(Cypress.env('gits_url')+'/'+Cypress.env('username'))
     })
   })
